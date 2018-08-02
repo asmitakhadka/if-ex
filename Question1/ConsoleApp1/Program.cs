cf91026a1458af9919c9cc169f32a584d92e459c
Asmita Khadka <10015309@student.toiohomai.ac.nz>
2018-08-02 14:44:57 +1200

    if exercise

---
+++ Question1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter another number");
+            int num2 = int.Parse(Console.ReadLine());
+
+           
+            if (num1 == num2)
+            Console.WriteLine("The both the numbers are equal.");
+       
+            if (num1 > num2)
+            Console.WriteLine("num1 is larger.");
+
+            else 
+            Console.WriteLine("num2 is larger");
+
+           
+        }
+    }
+}
---
+++ Question2/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int myNum = random.Next(1, 10);
+            
+            Console.WriteLine("Guess the number between 1 to 10");
+            int userNum = int.Parse(Console.ReadLine());
+
+            if (userNum == myNum)
+                Console.WriteLine("Your guess is right.");
+
+            else
+                Console.WriteLine("Your guess is wrong");
+               
+        }
+    }
+}
