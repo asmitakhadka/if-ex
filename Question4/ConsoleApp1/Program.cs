5c3e80ed6784e3bd690dc051f9916648a0c189a4
asmitakhadka <41553979+asmitakhadka@users.noreply.github.com>
2018-08-06 15:55:19 +1200

    Add files via upload

---
+++ Question4/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a integer");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("The number is even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd.");
+            }
+           
+
+        }
+    }
+}
