2c423a91555d15373371689a02f04c9f0fccabd2
asmitakhadka <41553979+asmitakhadka@users.noreply.github.com>
2018-08-06 16:42:41 +1200

    Add files via upload

---
+++ question5/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pin1 = 0;
+            int pin2 = 0;
+            Console.WriteLine("Enter a 4-digit PIN number.");
+            int pin = int.Parse(Console.ReadLine());
+
+            if (pin < 1000 || pin > 9999)
+            {
+                Console.WriteLine("This PIN number is not within the right parameters.");
+                 pin1 = int.Parse(Console.ReadLine());
+
+            }
+            else 
+            { 
+                Console.WriteLine("Please re-enter a PIN");
+                 pin2 = int.Parse(Console.ReadLine());
+            }
+            if (pin == pin2)
+            {
+                Console.WriteLine("Your PIN is set.");
+                pin2 = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Your PIN did not match.");
+            }
+        }
+    }
+}
