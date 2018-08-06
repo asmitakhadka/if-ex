ca8af00a746fd4d9c7a5bf77b71274a35ce602a5
asmitakhadka <41553979+asmitakhadka@users.noreply.github.com>
2018-08-06 16:33:38 +1200

    Add files via upload

---
+++ Questions3/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your final mark. ");
+            int i = int.Parse(Console.ReadLine());
+
+            if (i <= 90)
+            {
+                Console.WriteLine("Your grade is A+");
+                int num = Convert.ToInt32(Console.ReadLine());
+            }
+            else if(i >= 80 || i < 90)
+            {
+                Console.WriteLine("Your grade is A");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+            }
+            else if (i >= 70 || i < 80)
+            {
+                Console.WriteLine("Your grade is B+");
+               
+
+            }
+            else if (i >= 60 || i < 70)
+            {
+                Console.WriteLine("Your grade is B");
+          
+
+            }
+            else if (i >= 50 || i < 60)
+            {
+                Console.WriteLine("Your grade is C");
+                
+
+            }
+            else if (i < 50)
+            {
+                Console.WriteLine("Your grade is F");
+               
+            }
+        }
+    }
+}
