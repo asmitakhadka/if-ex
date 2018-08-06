255c24637966bcb69a001867864f569ab61671ec
asmitakhadka <41553979+asmitakhadka@users.noreply.github.com>
2018-08-06 15:37:19 +1200

    Update Program.cs

--- Question1/ConsoleApp1/Program.cs
+++ Question1/ConsoleApp1/Program.cs
@@ -16,15 +16,16 @@ namespace ConsoleApp1
             int num2 = int.Parse(Console.ReadLine());
 
            
-            if (num1 == num2)
-            Console.WriteLine("The both the numbers are equal.");
        
             if (num1 > num2)
             Console.WriteLine("num1 is larger.");
 
-            else 
+            else if (num2 > num1>
             Console.WriteLine("num2 is larger");
-
+            
+            
+            if (num1 == num2)
+            Console.WriteLine("The both the numbers are equal.");
            
         }
     }
