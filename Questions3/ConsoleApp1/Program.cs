aac0a09baeca7adc8f37d903ce282f52546b6ab2
asmitakhadka <41553979+asmitakhadka@users.noreply.github.com>
2018-08-08 08:38:47 +1200

    Update Program.cs

--- Questions3/ConsoleApp1/Program.cs
+++ Questions3/ConsoleApp1/Program.cs
@@ -10,41 +10,44 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int i;
             Console.WriteLine("Please enter your final mark. ");
-            int i = int.Parse(Console.ReadLine());
+            i = int.Parse(Console.ReadLine());
 
             if (i <= 90)
             {
                 Console.WriteLine("Your grade is A+");
-                int num = Convert.ToInt32(Console.ReadLine());
+
             }
-            else if(i >= 80 || i < 90)
+            else if (i >= 80 || i <= 89)
             {
                 Console.WriteLine("Your grade is A");
-                int num = Convert.ToInt32(Console.ReadLine());
 
             }
-            else if (i >= 70 || i < 80)
+            else if (i >= 70 || i <= 79)
             {
                 Console.WriteLine("Your grade is B+");
-               
 
             }
-            else if (i >= 60 || i < 70)
+            else if (i >= 60 || i <= 69)
             {
                 Console.WriteLine("Your grade is B");
-          
 
             }
-            else if (i >= 50 || i < 60)
+            else if (i >= 50 || i <= 59)
             {
                 Console.WriteLine("Your grade is C");
-                
+
+            }
+            else if (i >= 40 || i <= 49)
+            {
+                Console.WriteLine("Your grade id D");
 
             }
             else if (i < 50)
             {
                 Console.WriteLine("Your grade is F");
+            }
                
             }
         }
